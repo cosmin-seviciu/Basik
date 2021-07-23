8ffb92c8ab953527ed6be59f83c2c2803afdb0fd
Cosmin Seviciu <zetcoby@gmail.com>
2021-07-23 23:31:32 +0300

    wip

---
+++ src/services/fetchRepoService.ts
@@ -0,0 +1,36 @@
+import simpleGit, { SimpleGit, SimpleGitOptions } from 'simple-git';
+import * as fs from 'fs';
+import config from '../../basik.config.json';
+
+class FetchRepoService {
+  private git: SimpleGit;
+  private options: Partial<SimpleGitOptions> = {
+    baseDir: process.cwd(),
+    binary: 'git',
+    maxConcurrentProcesses: 6,
+  };
+  private remoteRepo: string = config.repoUrl;
+  private remoteRepoName: string = config.repoName;
+
+  constructor() {
+    this.git = simpleGit(this.options);
+    this.git.addRemote('origin', this.remoteRepo);
+    setInterval(() => {
+      this.getArticlesRepo();
+    }, 3600000);
+  }
+
+  getArticlesRepo() {
+    const localFiles = fs.readdirSync('./');
+
+    if (!localFiles.find((file) => file === this.remoteRepoName)) {
+      console.log('get repo');
+      this.git.clone(this.remoteRepo);
+    } else {
+      console.log('get pull');
+      this.git.pull();
+    }
+  }
+}
+
+export const fetchRepoService = new FetchRepoService();
